05997acbd96b70ce13e3e07e5bd50a1851cc2282
moiseberthe <75121872+moiseberthe@users.noreply.github.com>
2023-12-01 16:22:20 +0100

    Seance 3 API de prediction

--- client/app.py
+++ client/app.py
@@ -3,13 +3,56 @@ import json
 import requests
 
 with st.form("Insert fruit"):
-   st.write("Inside the form")
-   fruit = st.text_input("Fruit", '')
+    st.write("Fruit form")
+    fruit = st.text_input("Fruit", '')
 
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       # x = requests.post(url, json = myobj)
-       requests.post(f"http://localhost:8000/add/{fruit}")
-       fruits = requests.get("http://localhost:8000/list")
-       st.write("slider", fruits.text)
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        # Add new fruit 
+        requests.post(f"http://server:8000/add/{fruit}")
+
+        # get all fruits
+        fruits = requests.get("http://server:8000/list")
+        st.write(fruits.json())
+    
+with st.form("Predict Iris"):
+    st.write("Prediction")
+    
+    sepal_length = st.slider('Sepal length', 0, 10, 0)
+    sepal_width = st.slider('Sepal width', 0, 10, 0)
+
+    petal_length = st.slider('Petal length', 0, 10, 0)
+    petal_width = st.slider('Petal width', 0, 10, 0)
+
+    # Every form must have a submit button.
+    submitter = st.form_submit_button("Predict")
+
+    if submitter:
+        # call api
+        prediction = requests.post(f"http://server:8000/predict/{sepal_length}/{sepal_width}/{petal_length}/{petal_width}")
+        st.write(prediction.json())
+
+with st.form("Predict Penguin"):
+    st.write("Prediction")
+    
+    island = st.selectbox(
+        'Island',
+        ('Torgersen', 'Biscoe', 'Dream')
+    )
+    bill_length_mm = st.number_input('Bill length (mm)')
+    bill_depth_mm = st.number_input('Bill depth (mm)')
+    flipper_length_mm = st.number_input('Flipper length (mm)')
+    body_mass_g = st.number_input('body mass (g)')
+
+    sex = st.selectbox(
+        'sex',
+        ('male', 'female')
+    )
+    # Every form must have a submit button.
+    submitter = st.form_submit_button("Predict")
+
+    if submitter:
+        # call api
+        prediction = requests.post(f"http://server:8000/predict_penguins/{island}/{bill_length_mm}/{bill_depth_mm}/{flipper_length_mm}/{body_mass_g}/{sex}")
+        st.write(prediction.json())
--- server/app.py
+++ server/app.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+import pandas as pd
+import pickle
 
 app = FastAPI()
-client = MongoClient("localhost", 27017)
+
+# utiliser mongo à la place de localhost
+
+client = MongoClient("mongo", 27017)
 db = client.test_database
 collection = db.test_collection
 
+# load model
+with open("decision-tree.pkl", "rb") as f:
+    data = pickle.load(f)
+
+# load model
+with open("penguins-decision-tree.pkl", "rb") as f:
+    model_p = pickle.load(f)
+
+p_cols = ['island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']
+model = data["model"]
 
 @app.get("/")
 async def root():
@@ -18,3 +33,15 @@ async def get_fruits(my_fruit: str):
 @app.get("/list")
 async def list_fruits():
     return {"list": list(collection.find({}, {"_id": False}))}
+
+@app.post("/predict/{sepal_length}/{sepal_width}/{petal_length}/{petal_width}")
+async def predict(sepal_length,  sepal_width,  petal_length,  petal_width):
+    variety = model.predict(
+        [[sepal_length, sepal_width, petal_length, petal_width]]
+    )[0]
+    return {"prediction": variety}
+
+@app.post("/predict_penguins/{island}/{bill_length_mm}/{bill_depth_mm}/{flipper_length_mm}/{body_mass_g}/{sex}")
+async def predict_penguins(island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex):
+    specy = model_p.predict(pd.DataFrame([[island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex]], columns=p_cols))[0]
+    return {"prediction": specy}
